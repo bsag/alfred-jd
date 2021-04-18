8016a76b9b457177b54eecca3f9844659a157777
bsag <butshesagirl@rousette.org.uk>
2021-04-18 13:20:56 +0100

    Trying to fix updater function
    Also changed the workflow bundle id to match the name of the workflow better.

--- main.go
+++ main.go
@@ -11,10 +11,16 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"os/exec"
 
 	aw "github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/update"
 )
+
+// Name of the background job that checks for updates
+const updateJobName = "checkForUpdate"
 
 var (
 	query string
@@ -29,12 +35,15 @@ var (
 
 	// command line arguments
 	doAction, getLevel, catFolder string
+	doCheck                       bool
 
 	// Our Workflow object
 	wf *aw.Workflow
 )
 
 func init() {
+	flag.BoolVar(&doCheck, "check", false, "check for a new version")
+
 	// Initialise workflow and set up update mechanism
 	wf = aw.New(update.GitHub(repo), aw.HelpURL(repo+"/issues"))
 
@@ -52,6 +61,49 @@ func run() {
 	// Parse command-line flags and decide what to do
 
 	flag.Parse()
+
+	// Alternate action: Get available releases from remote.
+	if doCheck {
+		wf.Configure(aw.TextErrors(true))
+		log.Println("Checking for updates...")
+		if err := wf.CheckForUpdate(); err != nil {
+			wf.FatalError(err)
+		}
+		return
+	}
+
+	// Call self with "check" command if an update is due and a check
+	// job isn't already running.
+	if wf.UpdateCheckDue() && !wf.IsRunning(updateJobName) {
+		log.Println("Running update check in background...")
+
+		cmd := exec.Command(os.Args[0], "-check")
+		if err := wf.RunInBackground(updateJobName, cmd); err != nil {
+			log.Printf("Error starting update check: %s", err)
+		}
+	}
+
+	// Only show update status if query is empty.
+	if query == "" && wf.UpdateAvailable() {
+		// Turn off UIDs to force this item to the top.
+		// If UIDs are enabled, Alfred will apply its "knowledge"
+		// to order the results based on your past usage.
+		wf.Configure(aw.SuppressUIDs(true))
+
+		// Notify user of update. As this item is invalid (Valid(false)),
+		// actioning it expands the query to the Autocomplete value.
+		// "workflow:update" triggers the updater Magic Action that
+		// is automatically registered when you configure Workflow with
+		// an Updater.
+		//
+		// If executed, the Magic Action downloads the latest version
+		// of the workflow and asks Alfred to install it.
+		wf.NewItem("Update available!").
+			Subtitle("↩ to install").
+			Autocomplete("workflow:update").
+			Valid(false).
+			Icon(updateAvailable)
+	}
 
 	// choose the entry point to the different workflow branches
 	// search levels and act on candidates
